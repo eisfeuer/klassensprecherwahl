b22e844eee68c87205462be7771ac2eadbc63876
Niclas Schirrmeister <niclas@EISSTURM-LIN.(none)>
2015-06-05 09:54:33 +0200

    initiales projekt

---
+++ src/klassensprecherwahl/KlassensprecherwahlApp.java
@@ -0,0 +1,44 @@
+/*
+ * KlassensprecherwahlApp.java
+ */
+
+package klassensprecherwahl;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class KlassensprecherwahlApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new KlassensprecherwahlView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of KlassensprecherwahlApp
+     */
+    public static KlassensprecherwahlApp getApplication() {
+        return Application.getInstance(KlassensprecherwahlApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(KlassensprecherwahlApp.class, args);
+    }
+}
