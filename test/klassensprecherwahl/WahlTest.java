ab2c624931f46e31594728caa1b1192a45acce14
Niclas Schirrmeister <niclas@EISSTURM-LIN.(none)>
2015-06-05 10:38:42 +0200

    tests hinzugefuegt

--- test/klassensprecherwahl/WahlTest.java
+++ test/klassensprecherwahl/WahlTest.java
@@ -4,6 +4,7 @@
  */
 package klassensprecherwahl;
 
+import org.junit.Assert;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
@@ -13,4 +14,20 @@ import org.junit.Test;
  */
 public class WahlTest {
 
+    @Test
+    public void leereKandidatenListeBeiInit(){
+        Wahl wahl = new Wahl();
+        Kandidat[] erwartet = new Kandidat[0];
+        Assert.assertArrayEquals(erwartet, wahl.getKandidatenAsArray());
+    }
+    
+    public void addKandidatUndGetKandidat(){
+        Wahl wahl = new Wahl();
+        Kandidat k = new Kandidat("Olaf Scholz");
+        wahl.addKandidat(k);
+        Kandidat[] ergebnis = wahl.getKandidatenAsArray();
+        assertEquals(1, ergebnis.length);
+        assertEquals(k, ergebnis[0]);
+    }
+    
 }
